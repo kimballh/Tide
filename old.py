ba47a5c54251687a2f457f5369ab982d9188fe5c
Kimball Hill <hillkimball@gmail.com>
2018-04-12 06:39:13 -0600

    new commit

--- old.py
+++ old.py
@@ -143,7 +143,7 @@ class Main(wx.Frame):
         # self.bitmap1 = wx.StaticBitmap(self, -1, wx.Bitmap('3 place bottle.png'), (0,0))
         pic = wx.ImageFromBitmap(wx.Bitmap('4 Fill instructions.png'))
         pic = pic.Scale(self.display_length, self.display_height, wx.IMAGE_QUALITY_HIGH)
-        self.pic1 = wx.StaticBitmap(self.pnl, -1, wx.Bitmap(pic), (0, 0))
+        wx.StaticBitmap(self.pnl, -1, wx.Bitmap(pic), (0, 0))
         # self.pnl.Bind(wx.EVT_MOUSE_EVENTS, self.fill_st)
         self.move_to_5_button = wx.Button(self, label="MOVE", pos=(0, 0), size=(self.display_length, self.display_height))
         self.Bind(wx.EVT_BUTTON, self.fill_st, self.move_to_5_button)
@@ -152,13 +152,14 @@ class Main(wx.Frame):
 
     def fill_st(self, event=None):
         # GPIO.remove_event_detect(BUTTON)
-        # self.reset_panel()
+        self.move_to_5_button.Destroy()
+        self.reset_panel()
         print("I'm in fill_st")
 
         pic = wx.ImageFromBitmap(wx.Bitmap('5 Count up.png'))
         pic = pic.Scale(self.display_length, self.display_height, wx.IMAGE_QUALITY_HIGH)
         wx.StaticBitmap(self.pnl, -1, wx.Bitmap(pic), (0, 0))
-        self.pic1.Hide()
+        # self.pic1.Hide()
         font = wx.Font(120, wx.MODERN, wx.NORMAL, wx.BOLD)
         self.ounces_text = wx.StaticText(self.pnl, pos=((self.display_length / 3) * 1.85, self.display_height / 5.2))
         self.price_text = wx.StaticText(self.pnl, pos=((self.display_length / 3) * 1.85, self.display_height / 2.3))
